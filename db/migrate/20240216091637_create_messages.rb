3134861e2449e0ebc3b9b4348b82803c72aeec29
Umer Qaisar <umerqaisar786@gmail.com>
2024-02-16 14:18:23 +0500

    Created Message model

---
+++ db/migrate/20240216091637_create_messages.rb
@@ -0,0 +1,10 @@
+class CreateMessages < ActiveRecord::Migration[7.1]
+  def change
+    create_table :messages do |t|
+      t.string :context, default: ""
+      t.references :user
+      
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_16_074637) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_16_091637) do
+  create_table "messages", force: :cascade do |t|
+    t.string "context", default: ""
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
